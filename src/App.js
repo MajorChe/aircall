131701807f6990339b8660400c335306b654aea4
MajorChe <charithnanduri03@gmail.com>
2022-02-03 16:30:51 -0500

    add outer mobile box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Box, Flex, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { PhoneIcon } from '@chakra-ui/icons'
+import Body from "./components/Navbar";
 
 function App() {
   return (
     <>
+    {/* created outer box representing a mobile phone */}
+    <Box
+      p={5}
+      shadow='md'
+      borderWidth='1px'
+      flex='1'
+      borderRadius='md'
+      height={"600px"}
+      width={"450px"}
+      align="center"
+      margin={"auto"}
+      mt="20"
+      overflowY={"scroll"}
+    >
+      <HStack justifyContent={"center"} spacing="5">
+        <PhoneIcon/>
+        <Heading fontSize='4xl'>aircall</Heading>
+      </HStack>
+      <Text mt={4}>text</Text>
+    </Box>
     </>
   );
 }
