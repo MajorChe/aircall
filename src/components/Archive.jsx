b5ba37a3eee05959088fb6bca7d0a203fc1176e1
MajorChe <charithnanduri03@gmail.com>
2022-02-03 20:33:15 -0500

    add archive call data to archive component

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,10 +1,34 @@
-import { Heading } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Heading, HStack } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CallItem from './CallItem';
 
 const Archive = () => {
+  const [calls,setCalls] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://aircall-job.herokuapp.com/activities")
+    .then((response) => {
+      setCalls(response.data)
+    })
+    .catch((e) => console.log(e))
+  },[])
+
+  const callItemList = calls.map((singleCall) => {
+    return (
+    singleCall.is_archived === true && <CallItem
+    key={singleCall.id}
+    callData={singleCall}
+    />)
+  })
   return (
     <>
-      <Heading>Archived Calls</Heading>
+    {/* my is margin top and bottom*/}
+    <HStack justifyContent={"center"} spacing={5}>
+      <Heading fontSize={"2xl"} my={"20px"}>Archived Feed</Heading>
+      <Button colorScheme='red' variant='outline' size={"sm"}>Reset Calls</Button>
+      </HStack>
+      {callItemList}
     </>
   );
 };
